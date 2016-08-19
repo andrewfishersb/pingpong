2176e61a9ce1ab06f68c9606dcec9253838141d8
Andrew Fisher <andrewfishersb@gmail.com>
2016-08-19 16:22:24 -0700

    now only take numbers in the text box and have a picture of myself along side some other styling additions

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,31 @@ var pingPong = function(number){
   var countUpArray =[];
   for(var i =0;i<number;i++){
     countUpArray[i] = (i+1)%3===0 ? ((i+1)%5===0 ? '<img src="img/pingpong.jpg"/>' :'<img src="img/ping.jpg"/>' ): ((i+1)%5===0 ? '<img src="img/pong.png"/>' : (i+1));
-    }
+  }
   return countUpArray;
-}
-
-var rowCreation = function(number){
-  var numberOfRows = number/5;
-  numberOfRows= Math.ceil(numberOfRows);
-  return numberOfRows;
 }
 
 //Front end
 $(function(){
-  $("form").submit(function(event){
-    event.preventDefault();
-    var userInput = parseInt($("input#ping-pong").val());
-    var rows = rowCreation(userInput);
-    var pingPongArray = pingPong(userInput);
-    var pingPongArray = pingPong(userInput);
-    $(".output").text("").append("<li>"+pingPongArray[0]+"</li>");
-    for(var i =1;i<pingPongArray.length;i++){
-      $(".output").append("<li>"+pingPongArray[i]+"</li>");
+  $("#ping-pong").keypress(function(event){
+    var range = event.charCode;
+    if(!(range>47 && range<58)){
+      event.preventDefault();
     }
   });
+  $("form").submit(function(event){
+      event.preventDefault();
+      var userInput = parseInt($("input#ping-pong").val());
+      console.log(userInput);
+      if(userInput===undefined || userInput===NaN || userInput===null){
+        alert("Not a number please enter again");
+      }
+      var rows = rowCreation(userInput);
+      var pingPongArray = pingPong(userInput);
+      var pingPongArray = pingPong(userInput);
+      $(".output").text("").append("<li>"+pingPongArray[0]+"</li>");
+      for(var i =1;i<pingPongArray.length;i++){
+        $(".output").append("<li>"+pingPongArray[i]+"</li>");
+      }
+    });
 });
