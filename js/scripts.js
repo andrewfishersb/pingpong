e41f2405331c20678e844164f09338b776f5e727
Andrew Fisher <andrewfishersb@gmail.com>
2016-08-19 15:07:55 -0700

    add a picture to represent pingpong, made a seperate branch to play around with making each number into its own box in a table

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,8 @@
 var pingPong = function(number){
   var countUpArray =[];
   for(var i =0;i<number;i++){
-  countUpArray[i] = (i+1)%3===0 ? ((i+1)%5===0 ? 'pingpong' :'ping' ): ((i+1)%5===0 ? 'pong' : (i+1));
-  console.log(i);
-  }
+    countUpArray[i] = (i+1)%3===0 ? ((i+1)%5===0 ? '<img src="img/pingpong.jpg"/>' :'<img src="img/ping.jpg"/>' ): ((i+1)%5===0 ? '<img src="img/pong.png"/>' : (i+1));
+    }
   return countUpArray;
 }
 
@@ -21,33 +20,10 @@ $(function(){
     var userInput = parseInt($("input#ping-pong").val());
     var rows = rowCreation(userInput);
     var pingPongArray = pingPong(userInput);
-    $(".output").text("").append(pingPongArray[0]);
-    for(var i =1;i<pingPongArray.length-1;i++){
-      $(".output").append(pingPongArray[i]);
+    var pingPongArray = pingPong(userInput);
+    $(".output").text("").append("<li>"+pingPongArray[0]+"</li>");
+    for(var i =1;i<pingPongArray.length;i++){
+      $(".output").append("<li>"+pingPongArray[i]+"</li>");
     }
-    $(".output").append(pingPongArray[i]);
-    $("table").text("").append("<tr><td>hello</td></tr>");
-    for(var i =1;i<rows;i++){
-      $("table").append("<tr><td>hi</td></tr>");
-      for(var j =0;j<5;j++){
-
-       }
-    //
-     }//.append("<tr><td>hello</td></tr>"
-
   });
 });
-//original print out
-//   var pingPongArray = pingPong(userInput);
-//   $(".output").text("").append("<li>"+pingPongArray[0]+"</li>");
-//   for(var i =1;i<pingPongArray.length-1;i++){
-//     $(".output").append("<li>"+pingPongArray[i]+"</li>");
-//   }
-//   $(".output").append("<li>"+pingPongArray[i]+"</li>");
-
-
-//original if statement
-  // countUpArray[i] = (i+1)%3===0 ? ((i+1)%5===0 ? "Ping-Pong" :'<img src="img/ping.jpg"/>' ): ((i+1)%5===0 ? '<img src="img/pong.png"/>' : (i+1));
-
-//  countUpArray[i] = (i+1)%3===0 ? ((i+1)%5===0 ? '<img src="img/pingpong.jpg"/>' :'<img src="img/ping.jpg"/>' ): ((i+1)%5===0 ? '<img src="img/pong.png"/>' : (i+1));
-  //} --- doesnt work but has the last image entered
