ba593bbaf904ea982e5a8c79932eab656af28aec
Andrew Fisher <andrewfisher@Andrews-MacBook-Pro.local>
2016-08-23 18:54:22 -0700

    now seperate backend from front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,36 @@
 //Backend
 var pingPong = function(number){
   var countUpArray =[];
-  for(var i =0;i<number;i++){
-    countUpArray[i] = (i+1)%3===0 ? ((i+1)%5===0 ? '<img src="img/pingpong.jpg"/>' :'<img src="img/ping.jpg"/>' ): ((i+1)%5===0 ? '<img src="img/pong.png"/>' : (i+1));
+  for(var i =1;i<number+1;i++){
+    countUpArray[i-1] = (i)%3===0 ? ((i)%5===0 ? 'Ping-Pong' :'Ping' ): ((i)%5===0 ? 'Pong' : (i));
   }
   return countUpArray;
 }
 
 //Front end
 $(function(){
-  $("#ping-pong").keypress(function(event){
-    var range = event.charCode;
-    if(!(range>47 && range<58)){
-      event.preventDefault();
-    }
-  });
+
 
   $("form").submit(function(event){
       event.preventDefault();
+      $(".output").empty();
       var userInput = parseInt($("input#ping-pong").val());
-      console.log(userInput);
       if(userInput===undefined || userInput===NaN || userInput===null){
         alert("Not a number please enter again");
       }
       var pingPongArray = pingPong(userInput);
-      var pingPongArray = pingPong(userInput);
-      $(".output").text("").append("<li>"+pingPongArray[0]+"</li>");
-      for(var i =1;i<pingPongArray.length;i++){
+      for(var i =0;i<pingPongArray.length;i++){
+        console.log(pingPongArray[i]);
+        if(pingPongArray[i]==='Ping-Pong'){
+          pingPongArray[i]= "<img src='img/pingpong.jpg'/>"
+       }else if(pingPongArray[i]==='Ping'){
+          pingPongArray[i]= "<img src='img/ping.jpg'/>"
+        }else if(pingPongArray[i]==='Pong'){
+          pingPongArray[i]= "<img src='img/pong.png'/>"
+        }else{
+          pingPongArray[i]= i+1;
+        }
         $(".output").append("<li>"+pingPongArray[i]+"</li>");
       }
     });
-});
+ });
